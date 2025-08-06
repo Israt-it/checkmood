298332294246d4748f78ab200381b84bd143071d
Israt-it <isrujahan00@gmail.com>
2025-08-06 22:12:23 +0600

    Update1 SimpleMoodSelector.java
    project part1

--- SimpleMoodSelector/SimpleMoodSelector.java
+++ SimpleMoodSelector/SimpleMoodSelector.java
@@ -3,29 +3,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SimpleMoodSelector extends JFrame implements ActionListener {
+public class SimpleMoodSelector extends JFrame {
 
-    JRadioButton happyBtn, sadBtn, tiredBtn, excitedBtn, angryBtn;
-    JButton showBtn, restartBtn;
-    JLabel messageLabel;
+    JTextField nameField;
 
     public SimpleMoodSelector() {
         setTitle("Mood Selector");
-        setSize(450, 350);
+        setSize(500, 400);
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         getContentPane().setBackground(new Color(245, 255, 250));
 
-        JLabel titleLabel = new JLabel("Select Your Mood");
-        titleLabel.setBounds(140, 20, 200, 30);
+        JLabel titleLabel = new JLabel("Welcome to Mood Checker!");
+        titleLabel.setBounds(130, 10, 300, 30);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
         add(titleLabel);
 
+        JLabel nameLabel = new JLabel("Type your name:");
+        nameLabel.setBounds(60, 50, 120, 25);
+        nameLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+        add(nameLabel);
+
+        nameField = new JTextField();
+        nameField.setBounds(180, 50, 200, 25);
+        add(nameField);
+
         setVisible(true);
     }
-
-    public void actionPerformed(ActionEvent e) {}
 
     public static void main(String[] args) {
         new SimpleMoodSelector();
